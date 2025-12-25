27924ae8aaad009b55c7398539041907c1500985
Tom Reinartz <tomreinartz@Toms-MacBook-Pro.local>
2025-12-25 15:19:05 -0600

    fixed

--- week12/exercise1/src/App.jsx
+++ week12/exercise1/src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World Again!</h1>
     </div>
   );
 }
