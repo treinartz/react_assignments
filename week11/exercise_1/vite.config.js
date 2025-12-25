0ec6c32bbd97d8d26251201ab4012c51f120dd7d
Tom Reinartz <tomreinartz@Toms-MacBook-Pro.local>
2025-12-25 11:51:42 -0600

    Initial repo structure

---
+++ week11/exercise_1/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+    base: '/react_assignments/week11/exercise_1/',
+  plugins: [react()],
+})
